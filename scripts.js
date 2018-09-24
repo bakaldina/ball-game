55f2563dd9b1ff9bd34c351898d72f6218a5035b
Екатерина Бакалдина <e.bakaldina@city-mobil.ru>
2018-09-24 16:15:35 +0300

    edit

--- scripts.js
+++ scripts.js
@@ -3,21 +3,28 @@ let field = document.querySelector('#field');
 let ball = document.querySelector('#ball');
 
 field.onclick = function(event) {
+
     let maxX = field.clientWidth - ball.offsetWidth;
+
     let maxY = field.clientHeight - ball.offsetHeight;
 
     let fieldCoords = field.getBoundingClientRect();
+
     let x = event.clientX - fieldCoords.left - field.clientLeft  - (ball.offsetWidth / 2);
+
     let y = event.clientY - fieldCoords.top - field.clientTop  - (ball.offsetHeight / 2);
 
     x = Math.max(x, 0);
+
     y = Math.max(y, 0);
 
     x = Math.min(x, maxX);
+
     y = Math.min(y, maxY);
 
 
     ball.style.left = x + 'px';
+
     ball.style.top = y + 'px';
 
 };
